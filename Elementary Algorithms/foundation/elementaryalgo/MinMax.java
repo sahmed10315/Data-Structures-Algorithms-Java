72ca9c8473207671bd01542bff579414b8ada6b4
sahmed10315 <sahmed10315@gmail.com>
2016-07-31 19:28:30 -0500

    Initial upload

---
+++ Elementary Algorithms/foundation/elementaryalgo/MinMax.java
@@ -0,0 +1,25 @@
+package foundation.elementaryalgo;
+
+/* 
+ * Find minimum and maximum in an array 
+ * Time complexity = O(N) 
+ * */
+public class MinMax {
+	public static void main(String [] args) {
+		
+		int [] array = {44, 31, 14, 5, 22, 55, 234, 11, 54, 22}; 
+		
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		
+		for (int x : array) {
+			if (x < min) 
+				min = x;
+			if (x > max)
+				max = x;
+		}
+		
+		System.out.println("Minimum: " + min);
+		System.out.println("Maximum: " + max);
+	}
+}
