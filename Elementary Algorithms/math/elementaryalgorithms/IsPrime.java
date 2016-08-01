6617e79be1f8595fc3f845b674af9231fb08a713
sahmed10315 <sahmed10315@gmail.com>
2016-07-31 20:07:42 -0500

    Added source folders

--- Elementary Algorithms/math/elementaryalgorithms/IsPrime.java
+++ Elementary Algorithms/math/elementaryalgorithms/IsPrime.java
@@ -1,6 +1,21 @@
 package math.elementaryalgorithms;
 
+/* 
+ * Calculate prime of a number 
+ * Time complexity: O(sqrt(n))
+ * */
+
 public class IsPrime {
 	
-	boolean isPrime (int n)
+	public static void main(String [] args) {
+		System.out.println(isPrime(17));
+	}
+	
+	static boolean isPrime (int n) {
+		for (int x = 2; x <= Math.sqrt(n); x++) {
+			if (n % x == 0) 
+				return false;
+		}
+		return true;
+	}
 }
